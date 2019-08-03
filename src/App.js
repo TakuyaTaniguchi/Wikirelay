cf899f3bdd8fede74e65e0b21caf4a8fde040415
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-08-03 15:46:17 +0900

    Update render処理

--- src/App.js
+++ src/App.js
@@ -1,45 +1,69 @@
-import React, { Component } from 'react'
+import React from 'react'
+// import { render } from "react-dom"
 import firebase from './firebase'
 import './App.css'
 
 import axios from 'axios';
 
 
-class App extends Component {
-  state = {
-    user: null
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null,
+      urllist: [],
+      hoge: 'aaaa'
+    }
   }
 
   componentDidMount() {
+    console.log('moune')
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ user })
     })
   }
 
   login() {
+    console.log('login')
     const provider = new firebase.auth.GoogleAuthProvider()
     firebase.auth().signInWithRedirect(provider)
   }
 
   logout() {
+    console.log('logout')
     firebase.auth().signOut()
   }
-  getApi() {
-    const url = 'https://ja.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=links&titles=東京';
+  getApi = (url) => {
     axios
-    .get(url)
+    .get('https://ja.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=links&titles=東京')
     .then(res => {
       const data = res.data;
       const NextLinkKey = data.continue.plcontinue;
       const pgaeId = NextLinkKey.split('|')[0];
-      const links = data.query.pages[pgaeId];
-      return links;
+      const links = data.query.pages[pgaeId].links;
+      console.log(links)
+      this.setState({ hoge: 'bbbbb' });
     })
     .catch(error => {
       // 非同期処理失敗。呼ばれない
       console.log(error);
       console.log("dataError");
     });
+  }
+  list(list) {
+    console.log(list)
+    // const imageList = list.map(title => {
+    //   return (
+    //     <li>
+    //       {title}
+    //     </li>
+    //   );
+    // });
+    // return (
+    //   <ul>
+    //     <code>{imageList}</code>
+    //   </ul>
+    // );
   }
 
   render() {
@@ -59,6 +83,8 @@ class App extends Component {
           <li>
             <button onClick={this.getApi}>get</button>
           </li>
+          
+          <h2>{this.state.urllist}</h2>
         </ul>
       </div>
     )
